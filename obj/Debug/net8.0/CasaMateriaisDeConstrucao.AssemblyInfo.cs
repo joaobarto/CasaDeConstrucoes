ae37a6b3c510c0efe3af1f4f25b7e61fbb5d3a4b
joaobarto <joaobfn12@gmail.com>
2025-05-28 16:56:32 -0300

    Att

--- obj/Debug/net8.0/CasaMateriaisDeConstrucao.AssemblyInfo.cs
+++ obj/Debug/net8.0/CasaMateriaisDeConstrucao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasaMateriaisDeConstrucao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ddc86486451cce06c2a7daccd317af8fa8f21e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+910f5ae4309e209d169b5c789143dc6583e37911")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasaMateriaisDeConstrucao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasaMateriaisDeConstrucao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
